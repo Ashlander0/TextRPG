95e580dab2992a35d16b505535ac34d323d56754
Matthew <ashlander@live.com>
2020-10-19 00:15:38 -0500

    Reworked stat dictionaries

--- charcreate.py
+++ charcreate.py
@@ -1,121 +1,100 @@
 import os
-#     races -----------------
+import variables as v
+
+# races
 races = {}
-races["Human"] = {"index": "0",
-				"race": "Human",
+races[0] = {"race": "Human",
 				"Str": 15,
 				"End": 16,
 				"Dex": 15,
 				"Int": 15} 
-
-races["Elf"] = {"index": "1",
-				"race": "Elf",
+races[1] = {"race": "Elf",
 				"Str": 14,
 				"End": 14,
 				"Dex": 17,
 				"Int": 17}
- 
-races["Orc"] = {"index": "2",
-				"race": "Orc",
+races[2] = {"race": "Orc",
 				"Str": 17,
 				"End": 16,
 				"Dex": 14,
 				"Int": 12}
-#     classes ---------------
+
+# classes
 classes = {}
-classes["Warrior"] = {"Str": 2,
-					"End": 2,
-					"Dex": -1,
-					"Int": -2}
+classes[0] = {"title": "Warrior",
+				"Str": 2,
+				"End": 2,
+				"Dex": -1,
+				"Int": -2}
+classes[1] = {"title": "Mage",
+				"Str": -2,
+				"End": 0,
+				"Dex": 1,
+				"Int": 2}
+classes[2] = {"title": "Rogue",
+				"Str": -1,
+				"End": -1,
+				"Dex": 2,
+				"Int": 1}
 
-classes["Mage"] = {"Str": -2,
-					"End": 0,
-					"Dex": 1,
-					"Int": 2}
+# player stats
+playerstats = {
+	"Race": "",
+	"Class": "",
+	"Str": 0, 
+	"End": 0, 
+	"Dex": 0, 
+	"Int": 0}
 
-classes["Rogue"] = {"Str": -1,
-					"End": -1,
-					"Dex": 2,
-					"Int": 1}
-
-
-stats = {
-	"Strength", 
-	"Endurance", 
-	"Dexterity", 
-	"Intelligence"
-}
-
+# race selection function
 def raceselect():
-
-	global playerRace
-	global playerStr
-	global playerEnd
-	global playerDex
-	global playerInt
-	
 	os.system('cls')
 	print("===============================")
 	print("=====  Create  Charactor  =====")
 	print("===============================\n")
 
-# display
+	# display
 	racecount = 0
 	for x in races:
 		racecount = racecount + 1
-		print(f"[{racecount}] {x}")
+		print(f"[{racecount}] {races[x].get("race")}")
 
-# adjust raceinput		
+	# adjust raceinput		
 	raceinput = input("\nSelect your race. ")
 	raceinput = int(raceinput) - 1
 
-# set variables
+	# set variables
 	if races.get(str(raceinput)):
-		playerRace = races[str(raceinput)]['race']
-		playerStr = races[str(raceinput)]['Str']
-		playerEnd = races[str(raceinput)]['End']
-		playerDex = races[str(raceinput)]['Dex']
-		playerInt = races[str(raceinput)]['Int']
-		print(f"You are a {playerRace}, with \n{playerStr} Strength \n{playerEnd} Endurance \n{playerDex} Dexterity, and \n{playerInt} Intelligence")
-
-""" 	if raceinput == "1":
-		playerRace = races['Human']['race']
-		playerStr = races['Human']['Str']
-		playerEnd = races['Human']['End']
-		playerDex = races['Human']['Dex']
-		playerInt = races['Human']['Int']
-		print(f"You are a {playerRace}, with {playerStr} Str, {playerEnd} End, {playerDex} Dex, and {playerInt} Int.")
- """
+		playerstats["Race"] = races[raceinput]['race']
+		playerstats["Str"] = int(races[str(raceinput)]['Str'])
+		playerstats["End"] = races[(raceinput)]['End']
+		playerstats["Dex"] = races[str(raceinput)]['Dex']
+		playerstats["Int"] = races[str(raceinput)]['Int']
+	
+# class selection function
 def classselect():
-	global playerRace
-	global playerStr
-	global playerEnd
-	global playerDex
-	global playerInt
-	global playerClass
-
 	os.system('cls')
 	print("===============================")
 	print("=====  Create  Charactor  =====")
-	print("===============================\n")
+	print(f"================{playerstats}===============\n")
 
-# display
+	# display
 	classcount = 0
 	for clas in classes:
 		classcount = classcount + 1
 		print(f'[{classcount}] {clas}')
-		#print(f"You are a {playerRace}, with \n{playerStr} Strength \n{playerEnd} Endurance \n{playerDex} Dexterity, and \n{playerInt} Intelligence")
 
-# adjust classinput
+	# adjust classinput
 	classinput = input("\nSelect your class. ")
 	classinput = int(classinput) - 1
 
-# set variables
+	# set variables
 	if classes.get(str(classinput)):
-		playerClass = classes[str(classinput)]
-		playerStr = playerStr + classes[classinput]["Str"]
-		playerEnd = playerEnd + classes[classinput]["End"]
-		playerDex = playerDex + classes[classinput]["Dex"]
-		playerInt = playerInt + classes[classinput]["Int"]
+		playerstats["Class"] = classes[str(classinput)]["title"]
+		playerstats["Str"] = playerstats["Str"] + int(races[str(classinput)]['Str'])
+		playerstats["End"] = playerstats["End"] + races[(classinput)]['End']
+		playerstats["Dex"] = playerstats["Dex"] + races[str(classinput)]['Dex']
+		playerstats["Int"] = playerstats["Int"] + races[str(classinput)]['Int']
 
-print(f"You are a {playerRace} {playerClass}, with: \nStrength: {playerStr} \nEndurance: {playerEnd} \nDexterity: {playerDex} \nIntelligence: {playerInt}")
+	print(f"\nYou are a , with: \nStrength:  \nEndurance:  \nDexterity:  \nIntelligence: ")
+	print(playerstats["Str"])
--- main.py
+++ main.py
@@ -2,5 +2,4 @@ import functions as func
 import variables as v
 
 v.currentscreen = "main"
-func.mainmenu()
-  
+func.mainmenu() 
